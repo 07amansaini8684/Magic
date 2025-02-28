a5592c2fae1d478f225c80d44ec8adcc13bcfb65
07amansaini8684 <mr.amannobody@gmail.com>
2025-02-28 13:49:31 +0530

    done with particle js

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+import ParticlesBackground from './components/ParticleBackground'
+
+const App = () => {
+  return (
+    <div>
+      <ParticlesBackground />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/ParticleBackground.tsx
@@ -0,0 +1,103 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { useEffect, useMemo, useState } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import {
+  type Container,
+  type ISourceOptions,
+} from "@tsparticles/engine";
+import { loadSlim } from "@tsparticles/slim";
+
+const ParticlesBackground = () => {
+  const [init, setInit] = useState(false);
+
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      await loadSlim(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+
+  const particlesLoaded = async (container?: Container): Promise<void> => {
+    console.log(container);
+  };
+
+  const options: ISourceOptions = useMemo(
+    () => ({
+      background: {
+        color: {
+          value: "#000000",
+        },
+      },
+      fpsLimit: 120,
+      interactivity: {
+        events: {
+          onHover: {
+            enable: true,  // Enable mouse hover interaction
+            mode: "attract", // Use attract mode
+          },
+        },
+        modes: {
+          attract: {
+            distance: 100,  // Attraction range
+            duration: 0.3,  // Smooth movement
+            factor: 3,      // Strength of attraction
+          },
+        },
+      },
+      particles: {
+        number: {
+          value: 20,
+          density: {
+            enable: false,
+            value_area: 800,
+          },
+        },
+        color: {
+          value: "#ffffff",
+        },
+        shape: {
+          type: "circle",
+        },
+        opacity: {
+          value: 0.5,
+          random: true,
+        },
+        size: {
+          value: 1,
+          random: true,
+        },
+        move: {
+          enable: true,
+          speed: 1,
+          direction: "none",
+          random: true,
+          straight: false,
+          out_mode: "out",
+          bounce: false,
+          attract: {
+            enable: true,
+            rotateX: 300, // Adjust rotation attraction
+            rotateY: 300,
+          },
+        },
+      },
+      detectRetina: true,
+    }),
+    [],
+  );
+
+  if (init) {
+    return (
+      <Particles
+        id="tsparticles"
+        particlesLoaded={particlesLoaded}
+        options={options}
+      />
+    );
+  }
+
+  return <></>;
+};
+
+export default ParticlesBackground;
